c963014f7d48d45387fc5e5080d0f02992bea4b9
nathanwooo <nathanwooo1231@gmail.com>
2022-03-27 19:29:23 +0800

    minimap

---
+++ Cellsverse/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        Vector3 newPosition = player.position;
+        newPosition.z = transform.position.z;
+        transform.position = newPosition;
+    }
+}
