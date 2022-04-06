f7688083ddb296bfee71cc8e83645acb4bcc3671
nathanwooo <nathanwooo1231@gmail.com>
2022-04-07 01:08:03 +0800

    heart destrutable map

---
+++ Cellsverse/Assets/Scripts/DesroyBlock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DesroyBlock : MonoBehaviour
+{
+    private float offsetX;
+    private float offsetY;
+    private Tilemap DestructableTilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestructableTilemap = GetComponent < Tilemap >();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("trigger");
+        string name = collision.gameObject.name;
+        if (name == "bullet")//change to bullet later
+        {
+            Vector3 hitPosition = Vector3.zero;
+            foreach(ContactPoint2D hit in collision.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                DestructableTilemap.SetTile(DestructableTilemap.WorldToCell(hitPosition), null);
+            }
+
+        }
+    }
+}
