fb3b8da0713a1e5d6f2492215326de8e7add3a9a
nathanwooo <nathanwooo1231@gmail.com>
2022-04-07 00:03:51 +0800

    finsih red circle

--- Cellsverse/Assets/Scripts/dmgcircle.cs
+++ Cellsverse/Assets/Scripts/dmgcircle.cs
@@ -14,46 +14,65 @@ public class dmgcircle : MonoBehaviour
     private Vector3 targetCircleSize;
     private Transform charac;
     private float circleShrinkSpeed;
-    private float shrinkTimer;
+    private float shrinkTimer = 120;
+    private Vector3 targetCirclePosition;
+    [SerializeField] private Transform targetCircleTransform;
     private void Awake()
     {
-        circleShrinkSpeed = 5f;
+        circleShrinkSpeed = 20f;
         circleTransform = transform.Find("circle");
         topTransform = transform.Find("circle_top");
         bottomTransform = transform.Find("circle_bot");
         leftTransform = transform.Find("circle_left");
         rightTransform = transform.Find("circle_right");
-        charac = transform.Find("spaceship");
-        SetCircleSize(new Vector3(-0.1f,-3.9f),new Vector3(150, 150));
-        targetCircleSize = (new Vector3(0, 0));
+        //charac = transform.Find("spaceship");
+        SetCircleSize(new Vector3(-0.1f, -3.9f), new Vector3(150, 150));
+        SetTargetCircle(new Vector3(2.3f, 0.5f), new Vector3(20, 20));
+
     }
     private void Update()
     {
-        Vector3 sizeChangeVector = (targetCircleSize - circleSize).normalized;
-        Vector3 newCircleSize = circleSize + sizeChangeVector * Time.deltaTime * circleShrinkSpeed;
-        SetCircleSize(circlePosition, newCircleSize);
+        shrinkTimer -= Time.deltaTime;
+        if (shrinkTimer < 0)
+        {
+            Vector3 sizeChangeVector = (targetCircleSize - circleSize).normalized;
+            Vector3 newCircleSize = circleSize + sizeChangeVector * Time.deltaTime * circleShrinkSpeed;
+            Vector3 circleMoveDir = (targetCirclePosition - circlePosition).normalized;
+            Vector3 newCirclePosition = circlePosition + circleMoveDir * Time.deltaTime * circleShrinkSpeed;
+            SetCircleSize(newCirclePosition, newCircleSize);
+        }
+
     }
-    private void SetCircleSize(Vector3 position,Vector3 size)
+    private void SetCircleSize(Vector3 position, Vector3 size)
     {
         circlePosition = position;
         circleSize = size;
         transform.position = position;
         circleTransform.localScale = size;
         topTransform.localScale = new Vector3(15000, 11000);
-        topTransform.localPosition = new Vector3(0.3f, size.y * .5f + 45);
+        topTransform.localPosition = new Vector3(0.3f, size.y * .5f + 51);//45
 
         bottomTransform.localScale = new Vector3(15000, 11000);
-        bottomTransform.localPosition = new Vector3(0.3f, - size.y * .5f -45);
+        bottomTransform.localPosition = new Vector3(0.3f, -size.y * .5f - 58.7f);//45
 
         leftTransform.localScale = new Vector3(11000, 15000);
-        leftTransform.localPosition = new Vector3( - size.x * .5f -40 , -4.4f);
+        leftTransform.localPosition = new Vector3(-size.x * .5f - 55, -4.4f);//40
 
         rightTransform.localScale = new Vector3(11000, 15000);
-        rightTransform.localPosition = new Vector3(size.x * .5f+40, -4.4f);
+        rightTransform.localPosition = new Vector3(size.x * .5f + 54.8f, -4.4f);//40
     }
     private bool IsOutsideCircle(Vector3 position)
     {
         return Vector3.Distance(position, circlePosition) > circleSize.x * .5f;
     }
-    
+    private void SetTargetCircle(Vector3 position, Vector3 size)
+    {
+
+
+        targetCircleTransform.position = new Vector3(2.2f, -3.4f);
+        targetCircleTransform.localScale = new Vector3(3.9f, 3.9f);
+
+        targetCirclePosition = position;
+        targetCircleSize = size;
+    }
 }
