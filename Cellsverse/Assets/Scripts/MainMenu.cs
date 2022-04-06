814fb3bfc8b160c3b2869cf02277314eff73b565
hosuiting <u3556933@connect.hku.hk>
2022-04-06 20:48:24 +0800

    gg

---
+++ Cellsverse/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
