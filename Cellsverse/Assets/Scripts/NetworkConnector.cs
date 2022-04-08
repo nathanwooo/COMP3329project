6e162c6e9e6deb9830da511a7e3828fe97419630
nathanwooo <nathanwooo1231@gmail.com>
2022-04-08 21:08:49 +0800

    network connector

--- Cellsverse/Assets/Scripts/NetworkConnector.cs
+++ Cellsverse/Assets/Scripts/NetworkConnector.cs
@@ -3,6 +3,7 @@ using Photon.Pun;
 using Photon.Realtime;
 public class NetworkConnector : MonoBehaviourPunCallbacks
 {
+    public GameObject playerPrefab;
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -11,10 +12,26 @@ public class NetworkConnector : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to photon!");
+        // Try to join a random room
+        PhotonNetwork.JoinRandomRoom();
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarning($"Failed to connect: {cause}");
     }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // Failed to connect to random, probably because none exist
+        Debug.Log(message);
+        // Create a new room
+        PhotonNetwork.CreateRoom("Nat13213 Room");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"{PhotonNetwork.CurrentRoom.Name} joined!");
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 3.0f, 0), Quaternion.identity);
+
+    }
     #endregion
+
 }
