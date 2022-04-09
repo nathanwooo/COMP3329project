e28569311a7367abeb988e567d29a364e12c5aaa
hosuiting <u3556933@connect.hku.hk>
2022-04-09 20:13:33 +0800

    GG

--- Cellsverse/Assets/Scripts/LobbyManager.cs
+++ Cellsverse/Assets/Scripts/LobbyManager.cs
@@ -12,6 +12,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public GameObject roomPanel;
     public Text roomName;
 
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
+
     private void Start()
     {
         PhotonNetwork.JoinLobby();
@@ -33,5 +37,26 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(true);
         roomName.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateRoomList(roomList);
+    }
+
+    void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in roomItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        roomItemsList.Clear();
+
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
+        }
     }
 }
