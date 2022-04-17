32b4d2fb826a1add060c1dadad2732ab0d07752a
SumYg <61381909+SumYg@users.noreply.github.com>
2022-04-17 23:56:38 +0800

    Update DesroyBlock.cs

--- Cellsverse/Assets/Scripts/DesroyBlock.cs
+++ Cellsverse/Assets/Scripts/DesroyBlock.cs
@@ -30,24 +30,27 @@ public class DesroyBlock : MonoBehaviour
 
             if (name == "bullets_side(Clone)")//change to bullet later
             {
-                PV.RPC("DestroyMap", RpcTarget.All, collision);
+                var contacts = collision.contacts;
+                Vector3[] desPoints = new Vector3()[contacts.Length];
+                Vector3 hitPosition = Vector3.zero;
+                foreach (ContactPoint2D hit in contacts)
+                {
+
+                    hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                    hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+
+                    DestructableTilemap.SetTile(DestructableTilemap.WorldToCell(hitPosition), null);
+                }
+                PV.RPC("DestroyMap", RpcTarget.All, collision.contacts);
             }
         }
     }
     [PunRPC]
-    void DestroyMap(Collision2D collision)
+    void DestroyMap(ContactPoint2D[] contacts)
     {
         
 
-            Vector3 hitPosition = Vector3.zero;
-            foreach (ContactPoint2D hit in collision.contacts)
-            {
+            
 
-                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
-                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-
-                DestructableTilemap.SetTile(DestructableTilemap.WorldToCell(hitPosition), null);
-            }
-
-        }
+     }
 }
