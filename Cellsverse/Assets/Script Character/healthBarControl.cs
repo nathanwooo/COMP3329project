fded7db2655a99467dcbf2456e9aada53d5b479f
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2022-04-22 00:26:15 +0800

    a
    a

--- Cellsverse/Assets/Script Character/healthBarControl.cs
+++ Cellsverse/Assets/Script Character/healthBarControl.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 using System;
 public class healthBarControl : MonoBehaviour
 {
-    private GameObject cv, et, nm, hpBar, mpBar, exp, immueScore, bacteriaScore;//canvas, elite, name
-    private static GameObject lvCount;
+    [SerializeField] private GameObject cv, et, nm, hpBar, mpBar, exp, immueScore, bacteriaScore;//canvas, elite, name
+    [SerializeField] private GameObject lvCount;
     public static float maxHP;
     static float maxMP;
     public static float currentHP, currentMP, damage, extraDamage = 1, defense = 1f;
@@ -18,15 +18,15 @@ public class healthBarControl : MonoBehaviour
         maxHP = maxMP = currentHP = currentMP = 100;
         lv = 1;
         damage = maxHP/100 * 8f * extraDamage;
-        cv = GameObject.Find("immue(Clone)/Canvas");
-        et = GameObject.Find("immue(Clone)/Canvas/Elite");
-        nm = GameObject.Find("immue(Clone)/Canvas/Elite/Name");
-        lvCount = GameObject.Find("immue(Clone)/Canvas/Elite/Level/Text");
-        hpBar = GameObject.Find("immue(Clone)/Canvas/Elite/Bars/Healthbar");
-        mpBar = GameObject.Find("immue(Clone)/Canvas/Elite/Bars/Manabar");
-        exp =  GameObject.Find("immue(Clone)/Canvas/Expbar/exp");
-        immueScore = GameObject.Find("immue(Clone)/Canvas/scoreboard/immueTotalScore");
-        bacteriaScore = GameObject.Find("immue(Clone)/Canvas/scoreboard/bacteriaTotalScore");
+        // cv = GameObject.Find("immue(Clone)/Canvas");
+        // et = GameObject.Find("immue(Clone)/Canvas/Elite");
+        // nm = GameObject.Find("immue(Clone)/Canvas/Elite/Name");
+        // lvCount = GameObject.Find("immue(Clone)/Canvas/Elite/Level/Text");
+        // hpBar = GameObject.Find("immue(Clone)/Canvas/Elite/Bars/Healthbar");
+        // mpBar = GameObject.Find("immue(Clone)/Canvas/Elite/Bars/Manabar");
+        // exp =  GameObject.Find("immue(Clone)/Canvas/Expbar/exp");
+        // immueScore = GameObject.Find("immue(Clone)/Canvas/scoreboard/immueTotalScore");
+        // bacteriaScore = GameObject.Find("immue(Clone)/Canvas/scoreboard/bacteriaTotalScore");
         nm.GetComponent<Text>().text = "Immune";
         exp.GetComponent<Image>().fillAmount = 0f;
         nm.GetComponent<Text>().text = this.gameObject.name;
