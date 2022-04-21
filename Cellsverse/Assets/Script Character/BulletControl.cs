ec041a873b2d0d7da70ec9f0b95f337c5bf0e530
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2022-04-22 01:50:22 +0800

    dad
    ad

--- Cellsverse/Assets/Script Character/BulletControl.cs
+++ Cellsverse/Assets/Script Character/BulletControl.cs
@@ -8,20 +8,36 @@ public class BulletControl : MonoBehaviour{
     public GameObject hitEffect;
     public AudioClip explosionSound;
     healthBarControl HBControl;
+    PhotonView PV;
 
     void Start(){
+        PV = GetComponent<PhotonView>();
         HBControl = GetComponent<healthBarControl>();
         UpdateDamage();
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (PV.IsMine)
+        {
+            PV.RPC("enemyBulletCollision", RpcTarget.OthersBuffered);
+            // if (name == "Immue(Clone)")
+            // {//when more characters need change
+            //     HBControl.currentHP = HBControl.currentHP - damage;
+            // }
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+            Destroy(effect, 0.3f);
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+    }
+
+    [PunRPC]
+    void enemyBulletCollision()
+    {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         AudioSource.PlayClipAtPoint(explosionSound, transform.position);
         Destroy(effect, 0.3f);
-        PhotonNetwork.Destroy(gameObject);
-        if (collision.gameObject.name == "Immue(Clone)"){//when more characters need change
-            HBControl.currentHP = HBControl.currentHP - damage;
-        }
     }
 
     void Update(){
