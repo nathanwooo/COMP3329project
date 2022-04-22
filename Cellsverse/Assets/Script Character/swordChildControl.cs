3a2277e8734a555bdca0e0729f8fa05edec4ff36
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2022-04-22 21:22:34 +0800

    fa
    a

--- Cellsverse/Assets/Script Character/swordChildControl.cs
+++ Cellsverse/Assets/Script Character/swordChildControl.cs
@@ -9,6 +9,9 @@ public class swordChildControl : MonoBehaviour
         SWControl = this.transform.parent.parent.gameObject.GetComponent<swordControl>();
     }
     void OnTriggerEnter2D(Collider2D collision){
-        SWControl.OnTriggerEnter36D(collision);
+        if (collision.gameObject.name == "PlayerBoundary")
+        {
+           SWControl.OnTriggerEnter36D(collision); 
+        }
     }
 }
